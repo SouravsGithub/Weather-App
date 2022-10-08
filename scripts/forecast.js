3c25585f550701525eb2f15079a7d99b0d89f544
SouravsGithub <souravsemail2001@gmail.com>
2022-10-08 18:28:14 +0530

    wrote the whole logic to get the weather info from scratch

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,18 +1,20 @@
+// This is the key that api is using for authentication purposes
 const key = "o3atg97IcqFtP8nAdiVup7wRBuFcVGEB";
 
+// function to get information of the city
 const getCity = async (city) => {
   const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
-
   return data[0];
 };
 
-const getWeather = async (cityCode) => {
-  const base = `https://dataservice.accuweather.com/currentconditions/v1/`;
-  const query = `${cityCode}?apikey=${key}`;
+// function to get information of the weather in that city
+const getWeather = async (cityKey) => {
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${cityKey}?apikey=${key}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
@@ -20,6 +22,7 @@ const getWeather = async (cityCode) => {
   return data[0];
 };
 
+// calling the function and printing the information in the console
 getCity("kamakhyanagar")
   .then((data) => {
     return getWeather(data.Key);
@@ -27,7 +30,6 @@ getCity("kamakhyanagar")
   .then((data) => {
     console.log(data);
   })
-
   .catch((error) => {
     console.log(error);
   });
